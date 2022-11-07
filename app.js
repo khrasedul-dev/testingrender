85221712c104df4d99ce7785495cdd5217d1e1b3
KH Rasedul Islam <87899184+khrasedul-dev@users.noreply.github.com>
2022-11-07 19:24:01 +0600

    update

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ const bot = new Telegraf(process.env.TOKEN)
 
 const app = express()
 
+
+bot.start(ctx=>{
+    ctx.reply('The bot is started')
+})
+
+bot.command('test',ctx=>{
+    ctx.reply("This is test command")
+})
+
+app.use(bot.webhookCallback(`/${process.env.TOKEN}`))
+bot.telegram.setWebhook(`${process.env.DOMAIN}:8443/${process.env.TOKEN}`)
+
 app.get("/", (req, res) => {
     res.json({"status":"the site is working"})
 })
