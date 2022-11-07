af243b7efb76628494ab6912a8af4f176f4e3041
KH Rasedul Islam <87899184+khrasedul-dev@users.noreply.github.com>
2022-11-07 22:38:01 +0600

    update

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const {Telegraf} = require('telegraf')
+const { Telegraf } = require('telegraf')
+const mongoose = require('mongoose')
 
 
 const app = express()
@@ -7,20 +8,54 @@ const app = express()
 const bot = new Telegraf('5741034554:AAEVqOygMAETobk6FQbirjZdLWmwPKZQcWM')
 
 
+const s = new mongoose.Schema({
+    msg: {
+        type: String
+    }
+})
 
-bot.start(ctx=>{
+const testModel = mongoose.model('test', s)
+
+mongoose.connect(process.env.DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then((d) => console.log('Database connected'))
+    .catch((e) => console.log(e))
+
+
+
+
+bot.start(ctx => {
     ctx.reply('The bot is started')
 })
 
-bot.command('test',ctx=>{
-    ctx.reply("This is test command")
+bot.command('test', ctx => {
+    ctx.reply("This is test command not from db")
+})
+
+bot.command('testdb', ctx => {
+    testModel.find()
+        .then(data => {
+            ctx.reply(data[0].msg)
+        })
+        .catch(e => console.log(e))
 })
 
 app.use(bot.webhookCallback(`/`))
 
 app.get("/", (req, res) => {
-    res.json({"status":"the site is working"})
+    res.json({ "status": "the site is working" })
 })
+
+app.get("/data", (req, res) => {
+    testModel.find()
+        .then(data => {
+            res.json({ "Data": data[0].msg })
+        })
+        .catch(e => console.log(e))
+})
+
 
 const port = process.env.PORT || 5000
 
